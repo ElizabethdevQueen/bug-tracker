5691e31624b43d8eaba48fe57a1f8e926bd89dc6
Elizabeth Segun-Ajiboye <elizabethaderemi@gmail.com>
2021-04-05 03:02:45 +0100

    setting up the store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+import { bugAdded, bugResolved } from "./redux/action"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const unsubscribe = store.subscribe(() => {
+//   console.log("Store Changed!", store.getState())
+// })
+store.dispatch(bugAdded("Bug 1"));
+store.dispatch(bugResolved(1));
+
+// unsubscribe();
+
+// store.dispatch({
+//   type: actions.BUG_REMOVED,
+//   payload: {
+//     id: 1
+//   }
+// })
+console.log(store.getState())
---
+++ src/redux/actionType.js
@@ -0,0 +1,3 @@
+export const ADD_BUG = "bugAdded";
+export const BUG_REMOVED = "bugRemoved";
+export const BUG_RESOLVED = "bugResolved";
